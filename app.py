ca86d7dc83f1c71e2c4aaa4a320474aed20b67b3
Eduard289 <78046334+Eduard289@users.noreply.github.com>
2026-02-09 11:24:13 +0100

    Update headers and captions in app.py

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ st.markdown("""
 # --- BARRA LATERAL (CONEXIÓN CON REPO) ---
 with st.sidebar:
     st.image("https://cdn-icons-png.flaticon.com/512/2503/2503508.png", width=50)
-    st.header("🌐 Ecosistema ButacaVip")
+    st.header("Ecosistema ButacaVip")
     st.info("Este dashboard es una herramienta complementaria para el análisis de datos del addon oficial.")
     
     # EL BOTÓN AL REPO
     st.link_button("📥 Ir al Repositorio Kodi", "https://eduard289.github.io/repo-butacavip/#")
     
     st.divider()
-    st.caption("Desarrollado por Eduard289 © 2026")
+    st.caption("Desarrollado por Eduard289 -JL. As - © 2026")
 
 # --- CUERPO PRINCIPAL ---
 st.title("📊 ButacaVip: Analítica de Cine")
@@ -67,7 +67,7 @@ if busqueda or buscar:
                     c1, c2, c3 = st.columns(3)
                     c1.metric("Películas", len(df))
                     mej = df.loc[df['vote_average'].idxmax()]
-                    c2.metric("Mejor Valorada", mej['title'], f"{mej['vote_average']} ⭐")
+                    c2.metric("Mejor Valorada", mej['title'], f"{mej['vote_average']} ")
                     c3.metric("Promedio Saga", f"{round(df['vote_average'].mean(), 2)}")
                     
                     st.divider()
@@ -86,7 +86,7 @@ if busqueda or buscar:
                             if row.poster_path:
                                 st.image(BASE_URL + row.poster_path, use_container_width=True)
                             st.write(f"**{row.title}**")
-                            st.caption(f"{row.year} | ⭐ {row.vote_average}")
+                            st.caption(f"{row.year} |  {row.vote_average}")
             else:
                 st.error("No se encontraron resultados.")
 
